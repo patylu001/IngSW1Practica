e2a501494be6d99a4d3731b37b7c09140de521f0
Patricia Ortegon <patricia.ortegon@ibm.com>
2020-05-13 17:44:04 -0500

    # WARNING: head commit changed in the meantime
    Cambios pruebas unitarias

---
+++ src/main/java/com/fca/calidad/ArchivoNuevo.java
@@ -0,0 +1,14 @@
+package com.fca.calidad;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ArchivoNuevo {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
